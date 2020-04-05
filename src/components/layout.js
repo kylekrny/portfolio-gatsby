62614c536edcfe03ea9459fdfb2c73795a373534
kyledkearney <kyledkearney@gmail.com>
2020-04-05 15:40:07 -0600

    font awesome imported and working

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Menu from './menu'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import Header from "./header"
 import "./layout.css"
+library.add(fab)
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Menu = () => (
   <div id='menu'>
     <div id='menu-header'>
-    <p className='header-text'><a href="">Close</a></p>
-    <button id='mode-button'>Dark Mode</button>
+      <p className='header-text'><a href="">Close</a></p>
+    </div>
+    <div id='button-container'>
+      <button id='mode-button'>Dark Mode</button>
     </div>
     <nav id='menu-nav'>
-    <h6 className='menu-item'><a href="">Home</a></h6>
-    <h6 className='menu-item'><a href="">About</a></h6>
-    <h6 className='menu-item'><a href="">Experience</a></h6>
-    <h6 className='menu-item'><a href="">Work</a></h6>
-    <h6 className='menu-item'><a href="">Skills</a></h6>
+      <h6 className='menu-item'><a href="">Home</a></h6>
+      <h6 className='menu-item'><a href="">About</a></h6>
+      <h6 className='menu-item'><a href="">Experience</a></h6>
+      <h6 className='menu-item'><a href="">Work</a></h6>
+      <h6 className='menu-item'><a href="">Skills</a></h6>
     <div className='social-row'>
-
+    <FontAwesomeIcon icon={["fab", "instagram"]} className='social-icon' style={{color:"#000000"}} />
+    <FontAwesomeIcon icon={["fab", "github"]} className='social-icon' style={{color:"#000000"}} />
+    <FontAwesomeIcon icon={["fab", "behance"]} className='social-icon' style={{color:"#000000"}} />
     </div>
     </nav>
   </div>
