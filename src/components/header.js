242b06fa18512c42a31ae0920adebd3b0139dee8
kyledkearney <kyledkearney@gmail.com>
2020-04-05 14:21:32 -0600

    Header framed out & fonts imported

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,10 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div id='header-container'>
+      <p className='header-text desktop' id='header-brand'>Web Developer</p>
+      <p className='header-text' id='nav-link'>Menu</p>
     </div>
   </header>
 )
