0bedba0a25c872eed1387bfe94007aea2ab27f24
kyledkearney <kyledkearney@gmail.com>
2020-04-08 14:08:20 -0600

    parallax added to landing - basic function

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,19 @@ import Experience from "../components/mainPage/experience"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Work from "../components/mainPage/work"
+import { Parallax } from 'react-scroll-parallax';
 // import Skills from "../components/skills"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div id="landing">
-    <div><h1 id='landing-text-top' data-sal="slide-left" data-sal-duration="2000"  data-sal-easing="ease">Kyle</h1></div>
-    <div><h1 id='landing-text-bottom'data-sal="slide-right" data-sal-duration="2000"  data-sal-easing="ease" >Kearney</h1></div>
-    
+    <Parallax x={[35, -30]} tagOuter="figure">
+    <h1 id='landing-text-top'>Kyle</h1>
+    </Parallax>
+    <Parallax x={[-30, 30]} tagOuter="figure">
+    <h1 id='landing-text-bottom'>Kearney</h1>
+    </Parallax>
     </div>
     <div className="container">
         <Brief/>
