1755cd4a2b83d13c12b986ad0795e2a445e2072a
kyledkearney <kyledkearney@gmail.com>
2020-05-25 11:56:33 -0600

    updated colors to light mode / dark mode

--- src/components/mainPage/workCard.js
+++ src/components/mainPage/workCard.js
@@ -8,60 +8,12 @@ class WorkCard extends Component {
 
   render() {
 
-    const sideRender = (index, type) => {
-      const indexNum = parseInt((index + 1) % 2)
-      switch(type) {
-        case 'image':
-          switch (indexNum) {
-            case 0:
-              return 'even work-image-side'
-            default:
-              return 'odd work-image-side'
-          }
-        case 'text':
-          switch (indexNum) {
-            case 0:
-              return 'even work-text-side' 
-            default:
-              return 'odd work-text-side'
-          } 
-        case 'animate':
-          switch (indexNum) {
-            case 0:
-              return 'left' 
-            default:
-              return 'right'
-          } 
-        default :
-        return
-      }
-    }
-
 
     return (
       <div id='work-card-container'>
           {JSONData.MyWorkContent.map((data, index) => {
       return (
-       <div className="work-card">
-          <div index={index} className={sideRender(index, 'image')}>
-            <Fade delay={200}>
-              <div className="image-container1">
-                <Image filename={data.Images.Filename1}/>
-              </div>
-              <div className="image-container2">
-                <Image filename={data.Images.Filename2}/>
-              </div>
-            </Fade>
-          </div>
-          <Fade delay={200}>
-            <div className={sideRender(index, 'text')}>
-                  <p className='work-card-title'><a href={data.URL}>{data.Title}</a></p>
-                  <p className= 'work-card-subtitle'>{data.SubTitle}</p>
-                  <p className='work-card-description'>{data.Description}</p>
-            </div>
-          </Fade>
-        </div>
-
+          <span class='work-card-place-holder'></span>
       )})}
 
       </div>
