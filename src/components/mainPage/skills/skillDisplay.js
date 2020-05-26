4df00f4a67922eb993e6ef06de048665b7d7fb35
kyledkearney <kyledkearney@gmail.com>
2020-05-26 11:08:17 -0600

    setup switch cases

--- src/components/mainPage/skills/skillDisplay.js
+++ src/components/mainPage/skills/skillDisplay.js
@@ -3,7 +3,45 @@ import JSONData from '../../../data/content.json'
 
 export default class SkillDisplay extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeOption: JSONData.Skills[0].Type
+    };
+
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+  }
+
+  handleFilterClick(activeOption) {
+    this.setState({ activeOption });
+  }
+
+  renderSkills(data) {
+
+  }
+
+  handleFilter(option) {
+    switch (option) {
+        case 'Front End':
+          return
+        case 'Back End':
+          return 
+        case 'Dev Ops':
+          return
+        case 'Soft Skills':
+          return
+        case 'Still Installing':
+          return
+    }
+  }
+
+
+
+
   render() {
+
+    
     
     return (
       <div id="skill-display-container">
