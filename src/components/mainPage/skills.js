97a89b01c60da4de7f2c21fef7841a6c1fa61b97
kyledkearney <kyledkearney@gmail.com>
2020-05-27 19:17:18 -0600

    animated title

--- src/components/mainPage/skills.js
+++ src/components/mainPage/skills.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import SkillDisplay from './skills/skillDisplay'
-
+import Flip from 'react-reveal/Flip';
 
 export default class Skills extends Component {
 
@@ -8,8 +8,12 @@ export default class Skills extends Component {
     
     return (
       <div id="skills">
-        <h5 className='title-large skills-title'>Skills</h5>
-        <h5 className='skills-title-small'>To pay the bills</h5>
+        <Flip top>
+          <h5 className='title-large skills-title'>Skills</h5>
+        </Flip>
+        <Flip bottom>
+          <h5 className='skills-title-small'>To pay the bills</h5>
+        </Flip>
     
           <SkillDisplay/>
 
